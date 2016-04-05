4ccd9e52656b779d6de2033cf5e0bddedace7bc9
Sam Kreter <samkreter@gmail.com>
2016-04-05 17:13:09 -0500

    added base structure for the main function

---
+++ main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <map>
+#include <vector>
+#include <string>
+
+#include <mpi.h>
+
+
+int main(int argc, char * argv[])
+{
+
+    // Initialize MPI
+    // Find out my identity in the default communicator
+    MPI_Init(&argc, &argv);
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    // Master
+    if (rank == 0)
+    {
+        // ++++++++++++++++++++++++++++++
+        // Master process
+        // ++++++++++++++++++++++++++++++
+
+        // check the arugmumants
+        // if (argc < 2)
+        // {
+        //     std::cout << "Usage: " << argv[0] << std::endl;
+        //     return 1;
+        // }
+
+        //wait for the workers to finish to collect the results
+        MPI_Barrier(MPI_COMM_WORLD);
+
+    }
+    else
+    {
+        // ++++++++++++++++++++++++++++++
+        // Workers
+        // ++++++++++++++++++++++++++++++
+
+        //used for the barrier in master
+        MPI_Barrier(MPI_COMM_WORLD);
+    }
+
+    //Shut down MPI
+    MPI_Finalize();
+
+    return 0;
+
+} // END of main
+
--- mpiExamples/README.md
+++
@@ -1,2 +0,0 @@
-# Distributed-Vector-Distance-Calculator
-Calculates the L2 norm of a vector against a database of vectors using OpenMPI over a cluster and returns the top K closest matches
