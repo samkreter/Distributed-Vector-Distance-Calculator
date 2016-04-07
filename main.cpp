14c5973c6aa90a9d40a1af2da2621da59887be30
Sam Kreter <samkreter@gmail.com>
2016-04-07 17:28:17 -0500

    fixed the endless termination for certain filesizes

--- main.cpp
+++ main.cpp
@@ -4,21 +4,31 @@
 #include <string>
 #include <memory>
 #include <chrono>
+#include <string.h>
 
 #include "mpi.h"
 #include "include/Directory.hpp"
 #include "include/parser.hpp"
 
 #define MAX_MSG_SIZE 100
-#define MAX_RESULT_SIZE 100
+#define MAX_RESULT_SIZE 10
 
+//tags for OpenMPI
 #define TERMINATE 0
 #define FILENAME 1
 #define CMPVECTOR 2
 #define RESULTS 3
+#define KVALUE 4
 
-int sendWork(std::vector<std::string> fileNames);
-int doWork();
+typedef struct result{
+    float distance;
+    char fileName[MAX_MSG_SIZE];
+}result_t;
+
+
+int createMPIResultStruct(MPI_Datatype* ResultMpiType);
+int sendWork(std::vector<std::string> fileNames,MPI_Datatype ResultMpiType);
+int doWork(MPI_Datatype ResultMpiType);
 float findDist(int lineLength,std::shared_ptr<std::vector<float>> rawData,
     int startPos,std::vector<float> cmpVec);
 
@@ -33,6 +43,10 @@ int main(int argc, char * argv[])
     MPI_Init(&argc, &argv);
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+
+    MPI_Datatype ResultMpiType;
+    createMPIResultStruct(&ResultMpiType);
 
     Directory dir("/cluster_dev");
 
@@ -52,7 +66,7 @@ int main(int argc, char * argv[])
 
         auto test = dir.get_files();
         test.resize(2);
-        sendWork(test);
+        sendWork(test,ResultMpiType);
 
 
 
@@ -65,7 +79,7 @@ int main(int argc, char * argv[])
         // ++++++++++++++++++++++++++++++
         // Workers
         // ++++++++++++++++++++++++++++++
-        doWork();
+        doWork(ResultMpiType);
 
         //used for the barrier in master
         //MPI_Barrier(MPI_COMM_WORLD);
@@ -78,10 +92,12 @@ int main(int argc, char * argv[])
 
 } // END of main
 
-int sendWork(std::vector<std::string> fileNames){
+int sendWork(std::vector<std::string> fileNames,MPI_Datatype ResultMpiType){
 
     int threadCount;
     MPI_Comm_size(MPI_COMM_WORLD, &threadCount);
+    int fileCount = 0;
+
 
     std::vector<std::string>::iterator currFile = fileNames.begin();
 
@@ -101,23 +117,26 @@ int sendWork(std::vector<std::string> fileNames){
              FILENAME,         /* user chosen message tag */
              MPI_COMM_WORLD);   /* default communicator */
 
-
+        fileCount++;
     }
 
     for ( ;currFile != fileNames.end(); ++currFile) {
 
         // Receive results from a worker
-        char resultMsg[MAX_RESULT_SIZE];
+        result_t resultMsg[MAX_RESULT_SIZE];
         MPI_Status status;
 
+        std::cout << "waiting to recieve" << std::endl;
         // Receive a message from the worker
-        MPI_Recv(resultMsg,             /* message buffer */
+        MPI_Recv(&resultMsg,             /* message buffer */
             MAX_RESULT_SIZE,   /* buffer size */
-            MPI_CHAR,       /* data item is an integer */
+            ResultMpiType,       /* data item is an integer */
             MPI_ANY_SOURCE,     /* Recieve from thread */
             MPI_ANY_TAG,        /* tag */
                 MPI_COMM_WORLD,     /* default communicator */
                 &status);
+        fileCount--;
+        std::cout<<"recesived result: "<< resultMsg[0].distance << std::endl;
 
         //const int incomingIndex = status.MPI_TAG;
         const int sourceCaught = status.MPI_SOURCE;
@@ -134,9 +153,33 @@ int sendWork(std::vector<std::string> fileNames){
              sourceCaught,  /* destination process rank */
              FILENAME, /* user chosen message tag */
              MPI_COMM_WORLD);   /* default communicator */
+        fileCount++;
 
     }
 
+
+    for (int rank = 1; rank < threadCount && fileCount != 0; ++rank) {
+        // Receive results from a worker
+        result_t resultMsg[MAX_RESULT_SIZE];
+        MPI_Status status;
+
+        std::cout << "waiting to recieve1" << std::endl;
+        // Receive a message from the worker
+        MPI_Recv(&resultMsg,             /* message buffer */
+            MAX_RESULT_SIZE,   /* buffer size */
+            ResultMpiType,       /* data item is an integer */
+            MPI_ANY_SOURCE,     /* Recieve from thread */
+            MPI_ANY_TAG,        /* tag */
+                MPI_COMM_WORLD,     /* default communicator */
+                &status);
+        fileCount--;
+        const int sourceCaught = status.MPI_SOURCE;
+        std::cout<<"recesived result: "<< resultMsg[0].distance << "from: "<<sourceCaught << std::endl;
+        for(int i = 0; i < 5; i++){
+            std::cout<<resultMsg[i].fileName;
+        }
+        std::cout<<std::endl;
+    }
 
 
 
@@ -150,10 +193,16 @@ int sendWork(std::vector<std::string> fileNames){
     return 1;
 }
 
-int doWork(){
+//just a helper function for the sort with my custom structs
+bool resultPairSort(const result_t& pair1, const result_t& pair2){
+    return pair1.distance < pair2.distance;
+}
+
+int doWork(MPI_Datatype ResultMpiType){
 
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
 
     char msg[MAX_MSG_SIZE];
     MPI_Status status;
@@ -171,6 +220,8 @@ int doWork(){
                 MPI_COMM_WORLD,     /* default communicator */
                 &status);
 
+        const int k = 100;
+
         // Check if we have been terminated by the master
         // exit from the worker loop
         if (status.MPI_TAG == TERMINATE) {
@@ -183,7 +234,7 @@ int doWork(){
 
 
         std::shared_ptr<MapString_t> nameMap(new MapString_t);
-        std::map<float,std::string> results;
+        std::vector<result_t> results;
         std::shared_ptr<std::vector<float>> dataVector(new std::vector<float>);
         Parser p(nameMap,dataVector);
 
@@ -193,32 +244,33 @@ int doWork(){
             std::cerr<<"could not parse file: "<<msg<<std::endl;
             return 0;
         }
+        std::cout << "finished parsing the file" << std::endl;
 
         int lineLength = p.get_line_length();
+        int index = 0;
         for(auto& file : *nameMap){
-            results.insert(std::pair<float,std::string>(
-                findDist(lineLength,dataVector,file.second,*dataVector),
-                file.first));
+            std::cout <<"l";
+            results.push_back(result_t());
+            results.at(index).distance = findDist(lineLength,
+                dataVector,file.second,*dataVector);
+            strcmp(results.at(index).fileName,file.first.c_str());
         }
 
-        for(auto& el : results){
-            std::cout<<el.first<<":"<<el.second<<std::endl;
-        }
+        std::cout << "finished finding distances" << std::endl;
+
+        std::sort(results.begin(),results.end(),resultPairSort);
+        results.resize(MAX_RESULT_SIZE);
+
+        std::cout << "sending the final results" << std::endl;
 
 
-
-        const std::string result("Im done bro");
-        const size_t length = result.size();
-        char resultMsg[MAX_RESULT_SIZE];
-        result.copy(resultMsg,length);
-        resultMsg[length] = '\0';
-
-        MPI_Send(resultMsg,           /* message buffer */
+        MPI_Send(results.data(),           /* message buffer */
              MAX_RESULT_SIZE,         /* buffer size */
-             MPI_CHAR,              /* data item is an integer */
+             ResultMpiType,              /* data item is an integer */
              0,                     /* destination process rank, the master */
              RESULTS,             /* user chosen message tag */
              MPI_COMM_WORLD);
+        std::cout << "sent final results: "<<rank << std::endl;
     }
 
 
@@ -231,14 +283,36 @@ int doWork(){
 
 }
 
+int createMPIResultStruct(MPI_Datatype* ResultMpiType){
+    result_t value;
+    /** Type in order in the struct */
+    MPI_Datatype type[2] = { MPI_FLOAT, MPI_CHAR};
+    /** Number of occurence of each type */
+    int blocklen[2] = { 1, MAX_MSG_SIZE};
+    /** Position offset from struct starting address */
+    MPI_Aint disp[2];
+    disp[0] = reinterpret_cast<const unsigned char*>(&value.distance) - reinterpret_cast<const unsigned char*>(&value);
+    disp[1] = reinterpret_cast<const unsigned char*>(&value.fileName) - reinterpret_cast<const unsigned char*>(&value);
+
+    /** Create the type */
+    if(!MPI_SUCCESS == MPI_Type_create_struct(2, blocklen, disp, type, ResultMpiType)){
+        throw std::runtime_error("counted create the MPI result struct");
+    }
+    /** Commit it*/
+    if(!MPI_SUCCESS == MPI_Type_commit(ResultMpiType) ){
+        throw std::runtime_error("couldn't commit the MPI results struct");
+    }
+}
+
 float findDist(int lineLength,std::shared_ptr<std::vector<float>> rawData,int startPos,std::vector<float> cmpVec){
 
     float sum = 0;
+    float* rawd = (*rawData).data();
+    float* cmpRaw = cmpVec.data();
     //run the l1 norm formula
     for(int i = 0; i < lineLength; i++){
-        sum += std::fabs((*rawData).at(startPos+i) - cmpVec.at(i));
+        sum += std::fabs(*(rawd+startPos+i) - *(cmpRaw+i));
     }
-
     return sum / (float) lineLength;
 
 }
