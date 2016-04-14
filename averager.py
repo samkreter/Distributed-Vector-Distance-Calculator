18e4ff37e433e52ad705063daed1dd79d7b230ea
Sam Kreter <samkreter@gmail.com>
2016-04-14 08:55:52 -0500

    fixed the averageing python script

---
+++ averager.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+ks = [ 100, 500, 1000, 1500, 2000]
+ps = [ 8, 16, 24, 32, 40, 48]
+
+float_formatter = lambda x: "%.5f" % x
+np.set_printoptions(formatter={'float_kind':float_formatter})
+
+
+def takeAverageOfFile(filenameIn, filenameOut):
+    '''Load a csv and take the average of the sections
+         with the same value for first and second columns'''
+
+    data = np.genfromtxt(filenameIn,delimiter=',')
+    avgData = []
+
+    for k in ks:
+        for p in ps:
+            temp = data[np.logical_and(data[:,0] == p,data[:,1] == k)]
+            N = temp[:,0].size
+            tempSum = [np.sum(temp[:,i])/N for i in range(temp[0].size)]
+            avgData.append(tempSum)
+    finalData = np.array(avgData)
+    np.savetxt(filenameOut,finalData,delimiter=",",fmt='%.5f')
+    return finalData
+
+
+if __name__ == '__main__':
+    print(takeAverageOfFile("times.csv","averagedData.csv"))
